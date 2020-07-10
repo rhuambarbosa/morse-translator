4dc6067d4095326e7bebacff5eceea2ac8f27eb8
Rhuam Sousa <rhuam.sousa@belezanaweb.com.br>
2020-07-10 16:08:31 -0300

    add: initial structure

---
+++ src/test/java/br/com/rbs/translator/MorseTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rbs.translator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorseTranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
